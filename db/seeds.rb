e7495c6401415a4b6186bf2e0989811f90930e73
github username <github email address>
2020-12-28 20:27:12 -0500

    added faker image

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ category_choice1 = CategoryChoice.create!(name: "Sports")
 category_choice2 = CategoryChoice.create!(name: "Science")
 category_choice3 = CategoryChoice.create!(name: "Business")
 
-user1 = User.create!(name: "Bryam", username: "Bryam123", password_digest: "123", email: "Bryam123@gmail.com", image: "https://ca.slack-edge.com/T02MD9XTF-U019BJ3UY5B-d57d19806fbb-48")
+user1 = User.create!(name: "Bryam", username: "Bryam123", password_digest: "123", email: "Bryam123@gmail.com", image: Faker::Avatar.image(slug: "my-own-slug") )
 # user2 = User.create!(name: "Sabrina", username: "Sabrina123", password_digest: "123", email: "Sabrina123@gmail.com")
 
 fav1 = Favorite.create!(user_id: user1.id, name: "my lit list" )
